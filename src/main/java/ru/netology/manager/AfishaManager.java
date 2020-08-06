ce894b008340c84c5dd66e61529d8505d1cefbbc
OlgaDanilovaV <danilovaolga2235@gmail.com>
2020-08-07 00:12:31 +0300

    make more tests

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -1,9 +1,21 @@
 package ru.netology.manager;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.netology.domain.Movies;
 
+
+@Data
+@NoArgsConstructor
 public class AfishaManager {
 
+    private int defaultPoster = 10;
     private Movies[] afisha = new Movies[0];
+    private  int userRequestCount;
+
+    public AfishaManager(int userRequestCount) {
+        this.userRequestCount = userRequestCount;
+    }
 
     public void add(Movies film) {
         int length = afisha.length + 1;
@@ -29,14 +41,23 @@ public class AfishaManager {
     }
 
     public Movies[] showFilms() {
-        Movies[] lastFilms = new Movies[afisha.length];
-        int numberFilms = 10;
 
-        for (int i = 0; i < numberFilms; i++) {
-            int index = afisha.length - 1 - i;
-            lastFilms[i] = afisha[i];
+
+        int movieCount = afisha.length;
+
+        if (userRequestCount < 0) {
+            movieCount = defaultPoster;
         }
-        return lastFilms;
-    }
+        if (userRequestCount < movieCount) {
+            movieCount = userRequestCount;
+        }
 
+        Movies[] result = new Movies[movieCount];
+        for (int i = 0; i < movieCount; i++) {
+            int index = afisha.length - 1 - i;
+            result[i] = afisha[index];
+        }
+
+        return result;
+    }
 }
